857001ef024533bd3f47ca33256647502dfc39bd
hussainali7 <hussain@gmail.com>
2025-08-26 02:00:29 +0300

    add validation functions for password and email

---
+++ handlers/isValid.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"regexp"
+	_ "strings"
+	"unicode"
+)
+
+var emailRX = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[A-Za-z]{2,}$`)
+
+func IsValidPassword(pw string) bool {
+	if len(pw) < 8 {
+		return false
+	}
+	
+
+	var hasUpper, hasLower, hasDigit, hasSpecial bool
+	for _, ch := range pw {
+		switch {
+		case unicode.IsUpper(ch):
+			hasUpper = true
+		case unicode.IsLower(ch):
+			hasLower = true
+		case unicode.IsDigit(ch):
+			hasDigit = true
+		case regexp.MustCompile(`[!@#\$%\^&\*]`).MatchString(string(ch)):
+			hasSpecial = true
+		}
+	}
+	return hasUpper && hasLower && hasDigit && hasSpecial
+}
+
+func IsValidEmail(email string) bool {
+	return emailRX.MatchString(email)
+}
