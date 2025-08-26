4d88ed8fe2845bd8a5344b755473af881c4ac283
hussainali7 <h.anwdry@gmail.com>
2025-08-26 22:11:57 +0300

    Implement login functionality and add registration page template

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func main() {
 	// Routes
 	http.HandleFunc("/", handlers.HomeHandler)
 
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+
+		handlers.LoginHandler(w, r, database.DB)
+
+	})
+
 	// Start server
 	log.Println("Server running at http://localhost:7777")
 	err := http.ListenAndServe(":7777", nil)
