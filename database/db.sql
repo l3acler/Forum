8e179e641d7a9292aa7fb850497630b518a7a536
hussainali7 <hussain@gmail.com>
2025-08-29 12:57:53 +0300

    remove post_categories table definition from database schema

--- database/db.sql
+++ database/db.sql
@@ -54,16 +54,6 @@ CREATE TABLE IF NOT EXISTS categories (
     name TEXT NOT NULL UNIQUE
 );
 
---Post <-> Categories (many-to-many relation)
-
-CREATE TABLE IF NOT EXISTS post_categories (
-    post_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    PRIMARY KEY (post_id, category_id),
-    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
-);
-
 --Likes / Dislikes
 
 CREATE TABLE IF NOT EXISTS post_reactions (
