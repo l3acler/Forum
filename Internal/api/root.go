76ef4dfc285937681f1dff4244a97b67fc4d856b
hussainali7 <hussain@gmail.com>
2025-08-31 22:14:29 +0300

    refactor: enhance login and home page functionality with session management and dynamic post rendering

--- Internal/api/root.go
+++ Internal/api/root.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"forum/Internal/model"
 	"html/template"
 	"net/http"
 )
 
 func (server *Server) Get_RootHandler(w http.ResponseWriter, r *http.Request) {
+
+	sessionIDCookie, _ := r.Cookie("session_id")
+	
+
 	tmpl, tmplErr := template.ParseFiles("./web/templates/home.html")
 	if tmplErr != nil {
 		http.Error(w, "Error loading template", http.StatusInternalServerError)
@@ -19,6 +24,17 @@ func (server *Server) Get_RootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//if the user logged in this will be true
+	isLoggedIn := false
+	if sessionIDCookie != nil {
+		isLoggedIn = server.Service.IsValidSession(sessionIDCookie.Value)
+	}
+
+	pageData := model.PageData{
+		IsLoggedIn: isLoggedIn,
+		Posts:      posts,
+	}
+
 	// Pass posts to the template
-	tmpl.Execute(w, posts)
+	tmpl.Execute(w, pageData)
 }
