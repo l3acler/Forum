bef8cdc831d9f7aca1856798822b0db238708bbb
basayed <21327b.sa@gmail.com>
2025-09-03 04:52:13 +0300

    fixing

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 	"forum/Internal/api"
 	"forum/database"
 	"log"
+	//m "forum/Internal/model"
 )
 
 func main() {
 	// Initialize DB
 	DB, err := database.InitDB()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("DB init error:", err)
 	}
 
+	// Initialize service
 	service := api.NewService(DB)
+
+	// Initialize server
 	server := api.NewServer(7777, service)
-	fmt.Printf("Server running at http://localhost:%d \n", server.Port)
+
+	fmt.Printf("Server running at http://localhost:%d\n", server.Port)
 	log.Fatal(server.Start())
 }
