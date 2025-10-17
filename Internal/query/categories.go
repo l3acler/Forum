498927abd7d709f0c9c977e35792a7cd6bd2dc5f
Bader <alafoobader@gmail.com>
2025-10-17 19:40:13 +0300

    refactor: streamline category retrieval and error handling in service and API layers

--- Internal/query/categories.go
+++ Internal/query/categories.go
@@ -1,8 +1,13 @@
 package query
 
-import "database/sql"
+import (
+	"database/sql"
+	"forum/Internal/model"
+	"strings"
+)
 
 const selectCategoryIDByNameQ = `SELECT id FROM categories WHERE LOWER(name)=LOWER(?) LIMIT 1`
+const selectCategories = `SELECT * FROM categories`
 
 func SelectCategoryIDByName(db *sql.DB, name string) (int, error) {
 	var id int
@@ -11,4 +16,23 @@ func SelectCategoryIDByName(db *sql.DB, name string) (int, error) {
 		return 0, nil
 	}
 	return id, err
+}
+
+func GetCategories(db *sql.DB) ([]model.Category, error) {
+	rows, err := db.Query(selectCategories)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []model.Category
+	for rows.Next() {
+		var name string
+		var id int
+		if err := rows.Scan(&id, &name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, model.Category{Name: strings.ToLower(name), ID: id})
+	}
+	return categories, nil
 }
