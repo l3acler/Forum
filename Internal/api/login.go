b3720d9d7c5fbcc93cda1f80693bb85bf3608e91
hussainali7 <hussain@gmail.com>
2025-11-02 21:36:50 +0300

    last fix

--- Internal/api/login.go
+++ Internal/api/login.go
@@ -14,7 +14,7 @@ func (server *Server) Get_LoginHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("session_id")
 	if err == nil && server.Service.IsValidSession(cookie.Value) {
 		// User is already logged in
-		server.Service.LogoutUser(cookie.Value)
+		// server.Service.LogoutUser(cookie.Value)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
